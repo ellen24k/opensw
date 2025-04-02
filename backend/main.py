56d7762225f922b50dc6a3c41a2199ebc8ea549e
ellen24k <ellen24ks@gmail.com>
2025-04-03 03:28:52 +0900

    JSON data generation endpoints restore

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from fastapi import FastAPI, HTTPException, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from func.crawler import fetch_and_convert
 from func.mysql import import_csv_to_mysql, make_json, make_json_type1
 from func.data_processor import parse_org_time
-from func.redis import save_json_to_redis, get_json_from_redis, get_all_classroom_list, get_building_list
+from func.redis import save_json_to_redis, get_json_from_redis, get_all_classroom_list
 
 app = FastAPI(
     title="OpenSW API",
@@ -15,7 +16,7 @@ app = FastAPI(
 
 # CORS 미들웨어 추가
 app.add_middleware(
-    CORSMiddleware, # type: ignore
+    CORSMiddleware,  # type: ignore
     allow_origins=[
         "https://opensw.ellen24k.kro.kr",
         "https://opensw-dev.ellen24k.kro.kr",
@@ -52,32 +53,6 @@ async def run_crawler(background_tasks: BackgroundTasks):
         raise HTTPException(status_code=500, detail=result)
 
 
-# @app.get(
-#     "/make-json",
-#     summary="JSON 데이터 생성",
-#     description="MySQL에서 데이터를 가져와 JSON 형식으로 변환합니다.",
-# )
-# async def json_data():
-#     json_data = make_json()
-#     if json_data:
-#         return json.loads(json_data)
-#     else:
-#         raise HTTPException(status_code=500, detail="JSON 데이터를 생성하는 중 오류가 발생했습니다.")
-
-# @app.get(
-#     "/make-json-type1",
-#     summary="JSON 데이터 생성 (Type 1)",
-#     description="MySQL에서 데이터를 가져와 JSON 형식으로 변환합니다.",
-# )
-# async def json_data_type1():
-#     json_data = make_json_type1()
-#     if json_data:
-#         return json.loads(json_data)
-#     else:
-#         raise HTTPException(status_code=500, detail="JSON 데이터를 생성하는 중 오류가 발생했습니다.")
-
-
-# todo 캠퍼스별 건물명 그냥 크롤링으로 가져온 것 수동으로 분석 할 것. 혹은 그냥 죽전만 하는 것으로
 @app.get(
     "/save-to-redis",
     summary="데이터를 Redis에 저장",
@@ -106,6 +81,32 @@ async def save_to_redis():
         return {"message": "데이터가 Redis에 성공적으로 저장되었습니다."}
     else:
         raise HTTPException(status_code=500, detail="Redis에 데이터를 저장하는 중 오류가 발생했습니다.")
+
+
+@app.get(
+    "/make-json",
+    summary="전체 데이터를 JSON 형식으로 제공",
+    description="MySQL에서 데이터를 가져와 JSON 형식으로 변환합니다.",
+)
+async def json_data():
+    json_data = make_json()
+    if json_data:
+        return json.loads(json_data)
+    else:
+        raise HTTPException(status_code=500, detail="JSON 데이터를 생성하는 중 오류가 발생했습니다.")
+
+
+@app.get(
+    "/make-json-type1",
+    summary="전체 데이터를 JSON 형식으로 제공 (Type 1)",
+    description="MySQL에서 데이터를 가져와 JSON 형식으로 변환합니다.",
+)
+async def json_data_type1():
+    json_data = make_json_type1()
+    if json_data:
+        return json.loads(json_data)
+    else:
+        raise HTTPException(status_code=500, detail="JSON 데이터를 생성하는 중 오류가 발생했습니다.")
 
 
 @app.get(
@@ -229,20 +230,22 @@ async def query_building_table(building_id: str):
 async def query_classroom_list():
     return get_all_classroom_list()
 
+
 @app.get(
     "/query-classroom-list/{building_id}",
-    summary="특정 건물의 모든 강의실 리스트 정보 조회",
+    summary="특정 건물의 모든 강의실 리스트 정보 조회 예) 소프트",
     description=""
 )
 async def query_classroom_list_in_building(building_id=None):
-    classroom_list_in_building = get_all_classroom_list()
+    classroom_list = get_all_classroom_list()
 
     def filter_classroom_list(classroom_list, b_id):
         return {classroom for classroom in classroom_list if b_id in classroom}
 
-    classroom_list_in_building = filter_classroom_list(classroom_list_in_building, building_id)
+    classroom_list_in_building = filter_classroom_list(classroom_list, building_id)
 
     return sorted(classroom_list_in_building)
+
 
 @app.get(
     "/query-building-list",
@@ -250,4 +253,7 @@ async def query_classroom_list_in_building(building_id=None):
     description=""
 )
 async def query_building_list():
-    return get_building_list()
+    classroom_list = get_all_classroom_list()
+    ret_data = {re.sub(r'\d+(-\d+)?$', '', item) for item in classroom_list}
+    return sorted(ret_data)
+

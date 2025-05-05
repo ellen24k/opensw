9245300ef142e1dd7577b037d83ce21701dbe2f0
psmsq4 <shshin378@gmail.com>
2025-05-05 14:58:42 +0900

    .

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,12 @@
+import styles from "../styles/Footer.module.css";
+
 function Footer() {
     return (
         <div>
-            <footer>
+            <footer className={styles.Footer}>
                 <address>
                     더 많은 정보는 문의하세요.<br />
-                    <a href="mailto:psmsq4@dankook.ac.kr">단국대학교 소프트웨어학과 신승호</a>
+                    <a href="mailto:psmsq4@dankook.ac.kr">단국대학교 소프트웨어학과</a>
                 </address>
             </footer>
         </div>
--- frontend/src/components/IntroPage.js
+++ frontend/src/components/IntroPage.js
@@ -1,5 +1,4 @@
 import MainFrame from "./MainFrame.js"
-import ClassroomSchedule from "./ClassroomSchedule.js"
 import NaviBar from "./NaviBar.js"
 import { useState } from "react";
 

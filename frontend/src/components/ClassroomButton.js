8c4c42a308ece453357cb2899b0479909057e9fa
hsl0 <46296273+hsl0@users.noreply.github.com>
2025-05-12 00:10:39 +0900

    Fix ClassroomButton: 시간을 두 자리로 고정하여 표시하도록 수정하여 문자열을 통한 시간 비교시 안정성 보장

--- frontend/src/components/ClassroomButton.js
+++ frontend/src/components/ClassroomButton.js
@@ -6,7 +6,7 @@ import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import styles from '../styles/ClassroomButton.module.css';
 
 function ClassroomButton({ active, classroom, onClick, classes }) {
-    const currentTime = (date => `${date.getHours()}:${date.getMinutes()}`)(new Date());
+    const currentTime = (date => `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`)(new Date());
     const nextClass = classes
         ?.filter(cls => periodStart(cls.start) > currentTime)
         ?.sort((a, b) => a.start - b.start)?.[0];
--- frontend/src/util.js
+++ frontend/src/util.js
@@ -4,8 +4,8 @@
  * @returns {string} 시:분
  */
 export function minutesToTime(minutes) {
-    const hour = String(Math.floor(minutes / 60)).padStart(2, '0');
-    const minute = String(minutes % 60).padStart(2, '0');
+    const hour = Math.floor(minutes / 60).toString().padStart(2, '0');
+    const minute = (minutes % 60).toString().padStart(2, '0');
     return `${hour}:${minute}`;
 }
 

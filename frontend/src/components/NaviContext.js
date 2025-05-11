2492419ab574e89c55996de7268c66c137ba9d83
psmsq4 <shshin378@gmail.com>
2025-05-11 19:50:24 +0900

    psmsq4: Include Url Param: /viewClassScheudlePage/:classroomId && Maintain NaviContext

--- frontend/src/components/NaviContext.js
+++ frontend/src/components/NaviContext.js
@@ -3,8 +3,20 @@ import { useState, createContext } from "react";
 export const SelectedOptionIdContext = createContext(null);
 export const SelectedOptionIdSetterContext = createContext(null);
 
+function useLocalSelectedOptionId(value) {
+    const [selectedOptionId, setSelectedOptionId] = useState(
+        () => {
+            return localStorage.getItem("selectedOptionId") || value;
+        }
+    );
+    return [selectedOptionId, setSelectedOptionId];
+}
+
 export function SelectedOptionIdProvider({ children }) {
-    const [selectedOptionId, setSelectedOptionId] = useState(0);
+    const [selectedOptionId, setSelectedOptionId] = useLocalSelectedOptionId(0);
+    useEffect(() => {
+        localStorage.setItem("selectedOptionId", selectedOptionId);
+    }, [selectedOptionId]);
 
     return (
         <SelectedOptionIdContext.Provider value={selectedOptionId}>

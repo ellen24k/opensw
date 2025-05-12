037a68e4bde8280c81316a890c736a7629aea52e
ellen24k <ellen24ks@gmail.com>
2025-05-12 20:23:30 +0900

    Remove unused fetchClassroomSchedule function and related route from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { useEffect, useReducer } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-import ClassroomSchedule from './components/ClassroomSchedule';
 import IntroPage from './components/IntroPage.js';
 import FindEmptyClassPage from './components/FindEmptyClassPage.js';
 import ViewClassSchedulePage from "./components/ViewClassSchedulePage.js";
@@ -17,7 +16,6 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<IntroPage />} />
-            <Route path="/api-test" element={<ClassroomSchedule />} />
             <Route path="/FindEmptyClassPage" element={<FindEmptyClassPage />} />
             <Route path="/ViewClassSchedulePage" element={<ViewClassSchedulePage />} />
             <Route path="/ViewClassSchedulePage/:classroomName" element={<ViewClassSchedulePage />} />
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,15 +2,6 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export const fetchClassroomSchedule = async (classroomId) => {
-    try {
-        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/query-classroom/${classroomId}`);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-};
-
 let buildingListCache = null;
 
 export const fetchBuildingList = async () => {

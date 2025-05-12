2d32004eb60aef8be72171b4446bda8a298ee4d5
psmsq4 <shshin378@gmail.com>
2025-05-12 14:25:35 +0900

    psmsq4: Insert Comments in GanttChart.js

--- frontend/src/components/GanttChart.js
+++ frontend/src/components/GanttChart.js
@@ -72,10 +72,14 @@ const TableMap_ = [
 function GanttChart({ courses }) {
     const [TableMap, setTableMap] = useState(TableMap_)
 
+    // param 없음: 단순 조회를 위해 접속한 상황
+    // param 있음: [빈 강의실]에서 강의 시간표를 조회하는 상황 및 필터링을 통해 [검색]을 누른 상황
     useEffect(() => {
         if (courses == null) {
 
         } else {
+            // TableMap_으로 초기화하니 참조가 공유되는 문제가 있어 상태변경 미인식 및 강의실 시간표 중복문제가 발생해서 
+            // 매번 새로운 리터럴 객체를 생성한다.
             const newTableMap = [
                 { 9: ["", "", "", "", "", ""] },
                 { 9.5: ["", "", "", "", "", ""] },
@@ -106,15 +110,18 @@ function GanttChart({ courses }) {
                 { 22: ["", "", "", "", "", ""] },
                 { 22.5: ["", "", "", "", "", ""] },
             ]
+            // 강의 시간을 표준시간에 맞게 정규화한다.
             const normCourses = courses.map((courseObj, index) => {
                 const start = courseObj["start"]
                 const end = courseObj["end"]
                 return { ...courseObj, "course_code": parseInt(courseObj["course_code"]), "start": (start - 1) * 0.5 + 9, "end": (end - 1) * 0.5 + 9 }
             })
 
+            // 과목코드가 같은 강의는 같은 색상으로 처리하기 위해 고유 코드를 추출한다.
             const uniqueCourseCodes = [...new Set(normCourses.map(courseObj => courseObj["course_code"]))]
             console.log(uniqueCourseCodes)
 
+            // 강의실 별 색상을 지정한다. 과목코드가 같으면 같은 색상으로 처리한다.
             // forEach는 배열을 반환하지 않고, 콜백 함수만 처리한다. courseObj는 레퍼런스이므로 원본 배열을 변경할 수 있다.
             uniqueCourseCodes.map((code, idx) => {
                 normCourses.filter(courseObj => courseObj["course_code"] == code).forEach(courseObj => {
@@ -123,6 +130,7 @@ function GanttChart({ courses }) {
             })
 
             console.log(normCourses)
+            // 강의 시간을 표준시간에 맞게 정규화한 강의 배열을 순회하며 강의 시간표를 생성한다.
             normCourses.forEach((courseObj) => {
                 const courseCode = courseObj["course_code"]
                 const courseName = courseObj["course_name"]
@@ -130,15 +138,21 @@ function GanttChart({ courses }) {
                 const end = courseObj["end"]
                 const day = courseObj["day"]
 
+                // 강의 시간표를 생성한다.
+                // 강의 시간표는 2차원 배열로 생성된다.
+                // 첫 번째 차원은 시간대이며, 두 번째 차원은 요일이다.
+
                 newTableMap[(start - 9) * 2][start][DAY_LUT[day]] = courseObj
                 for (let i = start + 0.5; i <= end; i += 0.5) {
                     newTableMap[(i - 9) * 2][i][DAY_LUT[day]] = courseCode
                 }
             })
             console.log(newTableMap)
+            /* 생성된 강의 시간표를 상태변경한다. */
             setTableMap(newTableMap)
         }
 
+        // courses가 변경되면 강의 시간표를 생성한다. Parent Component에 종속되어 갱신된다.
     }, [courses])
 
     return (
@@ -154,11 +168,17 @@ function GanttChart({ courses }) {
             {TableMap.map((row, idx_row) => {
                 return (
                     <TableRow key={idx_row}>
+                        {/* 우선 헤더로 월~토까지 표시한다. */}
                         {(idx_row % 2 == 0) && <TableCell rowSpan={2} sx={{ borderRight: '1px solid rgba(224, 224, 224, 1)' }}>{hourHeader[idx_row / 2]}</TableCell>}
+
+                        {/* 각 시간대에 해당하는 강의 시간표를 그린다. */}
                         {Object.values(row)[0].map((course, index) => {
+                            {/* course가 빈 문자열이면 강의가 없다는 것이므로 빈 셀을 생성한다. */ }
                             if (course == "") {
                                 return <TableCell key={index} sx={{ borderRight: '1px solid rgba(224, 224, 224, 1)' }} />
-                            } else if (typeof (course) == "object") {
+                            }
+                            else if (typeof (course) == "object") {
+                                {/* course가 object이면 end-start+0.5만큼 rowspan을 가지는 셀을 생성한다. */ }
                                 return <TableCell
                                     key={index}
                                     sx={{
@@ -175,6 +195,7 @@ function GanttChart({ courses }) {
                                     P:{course.professor}
                                 </TableCell>
                             } else if (typeof (course) == "number") {
+                                {/* course가 number이면 위 course obj에 종속되어 span되었으므로 무시한다. */ }
                             }
                         })}
                     </TableRow>

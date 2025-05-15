dfc09f2ca8cb854a12526be4fd0c6b184ddfb206
psmsq4 <shshin378@gmail.com>
2025-05-16 00:50:04 +0900

    psmsq4: Dynamically Modifing colors of current courses to red when courses occupy their classroom in current time.

--- frontend/src/components/MainFrame.js
+++ frontend/src/components/MainFrame.js
@@ -3,14 +3,15 @@ import Header from "./Header.js";
 import Main from "./Main.js";
 import Footer from "./Footer.js";
 import styles from "../styles/MainFrame.module.css";
+import { Stack } from "@mui/material";
 
-function MainFrame({ children }) {
+function MainFrame({ children, spacing }) {
     return (
-        <div className={styles.MainFrame}>
+        <Stack className={styles.MainFrame}>
             <Header></Header>
-            <Main> {children} </Main>
+            <Main spacing={spacing}> {children} </Main>
             <Footer></Footer>
-        </div>
+        </Stack>
     )
 }
 
--- frontend/src/components/ViewClassSchedulePage.js
+++ frontend/src/components/ViewClassSchedulePage.js
@@ -2,20 +2,25 @@
 
 import MainFrame from "./MainFrame.js";
 import NaviBar from "./NaviBar.js";
-import { Button } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import ClassFilterArea from "./ClassFilterArea.js";
 import GanttChart from "./GanttChart.js";
 import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
+import ErrorIcon from '@mui/icons-material/Error';
 
 function ViewClassSchedulePage() {
     const [courses, setCourses] = useState(null);
     const { classroomName } = useParams();
 
     return (
-        <MainFrame>
+        <MainFrame spacing={1}>
             <NaviBar />
             <ClassFilterArea setCourses={setCourses} classroomName={classroomName}></ClassFilterArea>
+            <Stack direction="row" alignItems="center">
+                <ErrorIcon />
+                <Typography variant="body1">진행 중인 수업은 <span style={{ color: "#FF0000" }}>빨간색</span>으로 표시됩니다.</Typography>
+            </Stack>
             <GanttChart courses={courses}></GanttChart>
         </MainFrame>
     );

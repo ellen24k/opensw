6964e6554538338eb21cf6e75f79e97e78de407e
hsl0 <46296273+hsl0@users.noreply.github.com>
2025-05-17 20:31:28 +0900

    Implement BuildingFilter

--- frontend/src/components/BuildingFilter.js
+++ frontend/src/components/BuildingFilter.js
@@ -1,5 +1,50 @@
-function BuildingFilter({ selected, onChange }) {
+import { Fragment, use, useMemo } from 'react';
+import { fetchBuildingList } from '../api';
 
+import Card from 'react-bootstrap/Card';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Button from 'react-bootstrap/Button';
+
+import styles from '../styles/BuildingFilter.module.css';
+
+function BuildingFilter({ selected, onChange }) {
+    const buildings = use(useMemo(() => fetchBuildingList(), []));
+
+    const selectedSet = useMemo(() => new Set(selected), [selected]);
+    const unselected = useMemo(() => [...new Set(buildings).difference(selectedSet)], [buildings, selectedSet]);
+
+    function toggleBuilding(building) {
+        onChange([...selectedSet.symmetricDifference(new Set([building]))]);
+    }
+
+    return (
+        <Card>
+            <Card.Body className={styles.Buildings}>
+                <Dropdown>
+                    <Dropdown.Toggle variant="secondary" className={styles.Button}>건물</Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {' '}
+                        {unselected.map((building) => (
+                            <Dropdown.Item
+                                key={building}
+                                onClick={() => toggleBuilding(building)}
+                            >
+                                {building}
+                            </Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+                {selected.map((building) => (
+                    <div key={building}>
+                        <ToggleButton type="checkbox" checked className={styles.Button} onClick={() => toggleBuilding(building)}>
+                            {building}
+                        </ToggleButton>
+                    </div>
+                ))}
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default BuildingFilter;

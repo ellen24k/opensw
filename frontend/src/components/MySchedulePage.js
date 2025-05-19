d2b1a0b988e38c0a2ce54f0ce152b69b6a9723fe
wnstjd9288 <wnstjd9288@naver.com>
2025-05-19 20:55:01 +0900

    localStorage를 사용하여 페이지 새로고침 시에도 상태 유지

--- frontend/src/components/MySchedulePage.js
+++ frontend/src/components/MySchedulePage.js
@@ -3,18 +3,34 @@ import BottomSheet from './BottomSheet.js';
 import MainFrame from "./MainFrame.js";
 import NaviBar from "./NaviBar.js";
 import GanttChart from './GanttChart.js';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NowScheduleState } from './NowScheduleState.js';
 
 function MySchedulePage() {
-    const [courseList, setCoursesList] = useState([]);
+
+    const [isReset, setIsReset] = useState(false);
+    /*페이지 새로고침 시, courseList가 초기화 되는 부분을 방지하기 위해 localStorage 사용*/
+    const [courseList, setCoursesList] = useState(() => {
+        try {
+            const stored = localStorage.getItem('courseList');
+            return stored ? JSON.parse(stored) : [];
+        } catch (e) {
+            console.error('courseList 복원 실패:', e);
+            return [];
+        }
+    });
+    useEffect(() => {
+        localStorage.setItem('courseList', JSON.stringify(courseList));
+    }, [courseList]);
+
 
     return (
         <MainFrame>
             <NaviBar />
             <NowScheduleState courseList={courseList}></NowScheduleState>
             <GanttChart courses={courseList}></GanttChart>
-            <BottomSheet courseList={courseList} setCourseList={setCoursesList} />
+            <div style={{ height: "60px" }} />
+            <BottomSheet courseList={courseList} setCourseList={setCoursesList} isReset={isReset} setIsReset={setIsReset} />
         </MainFrame>
     );
 }
